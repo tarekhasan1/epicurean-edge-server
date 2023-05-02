b2b122ff228eb107a046beca13de3e9d9274ca40
Md. Tarek Hasan <tarekhasan.tkg101@gmail.com>
2023-05-02 19:33:04 +0600

    chefs data added to the server

--- index.js
+++ index.js
@@ -2,9 +2,18 @@ const express = require('express');
 const app = express();
 const port =process.env.PORT || 5000;
 
+
+const chefs = require('./data/chefs.json');
+
 app.get('/', (req, res)=>{
     res.send('data is coming')
 });
+
+
+app.get('/chefs', (req, res) =>{
+    res.send(chefs);
+})
+
 
 app.listen(port, ()=>{
     console.log(`the app is running on port: ${port}`);
